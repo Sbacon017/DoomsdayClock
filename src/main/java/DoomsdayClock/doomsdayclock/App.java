12161e078294d786677c7b14cf77e564a23fa150
Steph Bacon <sbacon017@gmail.com>
2018-03-27 09:35:07 -0400

    Initial commit

---
+++ src/main/java/DoomsdayClock/doomsdayclock/App.java
@@ -0,0 +1,13 @@
+package DoomsdayClock.doomsdayclock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
